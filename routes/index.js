8501756f613da666ecae681d5069f5a321ba3710
zhangzongzheng <zhangzongzheng@ymatou.com>
2016-05-12 22:52:03 +0800

    add archive

--- routes/index.js
+++ routes/index.js
@@ -126,7 +126,8 @@ app.get('/post', function(req, res) {
 app.post('/post', checkLogin);
 app.post('/post', function(req, res) {
     var currentUser = req.session.user,
-        post = new Post(currentUser.name, req.body.title, req.body.post);
+        tags = [req.body.tag1, req.body.tag2, req.body.tag3],
+        post = new Post(currentUser.name, req.body.title, tags, req.body.post);
     post.save(function(err) {
         if (err) {
             req.flash('error', err);
@@ -137,32 +138,81 @@ app.post('/post', function(req, res) {
     })
 });
 
-app.get('/u/:name', function(req, res) {
-    var page = parseInt(req.query.p) || 1;
-      //检查用户是否存在
-      User.get(req.params.name, function (err, user) {
-        if (!user) {
-          req.flash('error', '用户不存在!'); 
-          return res.redirect('/');
-        }
-        //查询并返回该用户第 page 页的 10 篇文章
-        Post.getTen(user.name, page, function (err, posts, total) {
-          if (err) {
-            req.flash('error', err); 
+app.get('/archive', function(req, res) {
+    Post.getArchive(function(err, posts) {
+        if (err) {
+            req.flash('error', err);
             return res.redirect('/');
-          } 
-          res.render('user', {
-            title: user.name,
+        }
+        res.render('archive', {
+            title: '存档',
             posts: posts,
-            page: page,
-            isFirstPage: (page - 1) == 0,
-            isLastPage: ((page - 1) * 10 + posts.length) == total,
             user: req.session.user,
             success: req.flash('success').toString(),
             error: req.flash('error').toString()
-          });
         });
-      }); 
+    });
+});
+
+app.get('/tags', function (req, res) {
+  Post.getTags(function (err, posts) {
+    if (err) {
+      req.flash('error', err); 
+      return res.redirect('/');
+    }
+    res.render('tags', {
+      title: '标签',
+      posts: posts,
+      user: req.session.user,
+      success: req.flash('success').toString(),
+      error: req.flash('error').toString()
+    });
+  });
+});
+
+app.get('/tags/:tag', function (req, res) {
+  Post.getTag(req.params.tag, function (err, posts) {
+    if (err) {
+      req.flash('error',err); 
+      return res.redirect('/');
+    }
+    res.render('tag', {
+      title: 'TAG:' + req.params.tag,
+      posts: posts,
+      user: req.session.user,
+      success: req.flash('success').toString(),
+      error: req.flash('error').toString()
+    });
+  });
+});
+
+
+app.get('/u/:name', function(req, res) {
+    var page = parseInt(req.query.p) || 1;
+    //检查用户是否存在
+    User.get(req.params.name, function(err, user) {
+        if (!user) {
+            req.flash('error', '用户不存在!');
+            return res.redirect('/');
+        }
+        //查询并返回该用户第 page 页的 10 篇文章
+        Post.getTen(user.name, page, function(err, posts, total) {
+            if (err) {
+                req.flash('error', err);
+                return res.redirect('/');
+            }
+            res.render('user', {
+                title: user.name,
+                posts: posts,
+                page: page,
+                isFirstPage: (page - 1) == 0,
+                isLastPage: ((page - 1) * 10 + posts.length) == total,
+                user: req.session.user,
+                success: req.flash('success').toString(),
+                error: req.flash('error').toString()
+            });
+        });
+    });
 });
 
 app.get('/u/:name/:day/:title', function(req, res) {
